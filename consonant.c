a4e3a18bc5ce32efcbfb1761a2cfdd732f57f28c
Dineshero <36227792+Dineshero@users.noreply.github.com>
2018-02-07 16:34:02 +0530

    Vowels or consonant

---
+++ consonant.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+char a;
+printf("Enter a character or string \n");
+scanf("%s",&a);
+if(a=='a' || a== 'e' || a=='i' || a=='o' || a=='u')
+{
+printf("\n Character");
+}
+else
+{
+printf("Consonant");
+}
+}
