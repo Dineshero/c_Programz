b2ffa4c80eeaca5b376c5f0e13bcbb9047b7e41c
Dineshero <36227792+Dineshero@users.noreply.github.com>
2018-02-07 16:25:34 +0530

    Constant or vowels

---
+++ constant.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int a;
+printf("Enter a character or string \n");
+scanf("%d",&a);
+if(n/1==n)
+{
+printf("\n Constant");
+}
+else
+{
+printf('\n Character");
+}
+}
