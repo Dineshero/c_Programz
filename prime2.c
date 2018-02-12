edeb83486e00ec5bb3a2af5591898c6e3cdbcef8
Dineshero <36227792+Dineshero@users.noreply.github.com>
2018-02-12 16:19:46 +0530

    Create prime2.c

---
+++ prime2.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main()
+{
+int low,high,i,fl;
+printf("The prime numbers series");
+printf("Enter the starting and ending values");
+scanf("%d\n%d\n",&low,&high);
+if(low<high)
+{
+fl=0;
+for(i=2;i<low/2;++i)
+{
+if(low%i==0)
+{
+fl=1;
+break;
+}}
+if(flag==0)
+printf("%d",low);
+++low
+}
+}
