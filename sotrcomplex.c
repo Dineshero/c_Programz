0f47356127348dc50332e3afb75e39753be64b08
Dineshero <36227792+Dineshero@users.noreply.github.com>
2018-02-19 16:15:55 +0530

    Create sotrcomplex.c

---
+++ sotrcomplex.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(){
+    int i,j,l,a[100],num,t;
+    scanf("%d",&n);
+    for(i=1;i<=num;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(j=1;j<=num;j++)
+    {
+        for(l=j+1;l<=num;l++)
+        {
+            if(a[j]>a[l])
+            {
+                t=a[j];
+                a[j]=a[l];
+                a[l]=t;
+            }
+        }
+    }
+printf("%d",a[j/2]);
+}
