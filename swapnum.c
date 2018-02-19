a6f5570ce6415bbcb479d93ff71a2d7b1a712f72
Dineshero <36227792+Dineshero@users.noreply.github.com>
2018-02-19 16:43:57 +0530

    Swapping the two numbers

---
+++ swapnum.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int a,b,temp=0;
+printf("Enter the two values to swap \n");
+scanf("%d %d",&a,&b);
+temp=a;
+a=b;
+b=temp;
+
+printf("The swapped values are \n");
+printf("%d %d",a,b);
+}
