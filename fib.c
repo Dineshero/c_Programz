96ef0cec2800418e16611982651ec39b7854dcd2
Dineshero <36227792+Dineshero@users.noreply.github.com>
2018-02-19 16:52:20 +0530

    Create fib.c

---
+++ fib.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int n,i,a=0,b=1,c;
+printf("The Fibonacci series is \n");
+printf("The range of fiboncci series \n");
+scanf("%d",&n);
+printf("\n%d\n%d",a,b);
+for(i=0;i<=(n-2);i++)
+{
+c=a+b;
+printf("\n%d",c);
+a=b;
+b=c;
+}
+}
