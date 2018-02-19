ab888086891c05f1bac86cc41ce4567a337d666c
Dineshero <36227792+Dineshero@users.noreply.github.com>
2018-02-19 15:59:56 +0530

    Number or not

---
+++ number.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char s[100],i=0;
+printf("Enter the string \n");
+scanf("%s",s);
+if(s[i] >= '0' && s[i] <='9')
+	printf("yes \n");
+	else
+	printf("no");
+	
+	
+}
