0ed716625a22afd97300f6914c77a574bbebc0c0
Dineshero <36227792+Dineshero@users.noreply.github.com>
2018-02-21 16:17:05 +0530

    Count the words in the string

---
+++ countword.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char s[100],i=0,count=0;
+printf("Enter the string \n");
+scanf("%[^\n]s",s);
+for(i=0;s[i]!='\0';i++)
+{
+if(s[i]==' ' || s[i]=='.' || s[i]==',')
+{count++;
+	
+}	
+}printf("%d",count+1);}
