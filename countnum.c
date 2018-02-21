22c095cee102795f02842660d63db6360d0405d4
Dineshero <36227792+Dineshero@users.noreply.github.com>
2018-02-21 16:06:16 +0530

    Count the number of digits in the string

---
+++ countnum.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char s[100],i=0,count=0;
+printf("Enter the string \n");
+scanf("%[^\n]s",s);
+for(i=0;s[i]!='\0';i++)
+{
+if(s[i] >= '0' && s[i] <='9')
+{count++;
+	
+}	
+}printf("%d",count);}
