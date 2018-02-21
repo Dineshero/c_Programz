6413bba7b42374c607722c8df4fa5bc29acf7ab7
Dineshero <36227792+Dineshero@users.noreply.github.com>
2018-02-21 16:38:13 +0530

    The given string will be print n times

---
+++ ntimesprint.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+int i,n;
+char str;
+printf("N value");
+scanf("%d",&n);
+printf("\n Enter string");
+scanf("%s",str);
+for(i=0;i<n;i++)
+{
+printf("\n %s",str);
+}
+}
