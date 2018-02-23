f8202243ad4a6a669e88fb1a157bd5e375b2475f
Dineshero <36227792+Dineshero@users.noreply.github.com>
2018-02-23 15:34:10 +0530

    Str concadination without using strcat function

---
+++ strcatwithout.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include<string.h>
+int main()
+{
+   char s1,s2,s3;
+   printf("String 1 \n");
+   scanf("%s",s1);
+   printf("String 2 \n");
+   scanf("%s",s2);
+   s3=s2+s1;
+   printf("%s",s3);
+}
