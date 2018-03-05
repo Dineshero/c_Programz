06f3c68032b086996b1bdf29d8308599760c8314
Dineshero <36227792+Dineshero@users.noreply.github.com>
2018-03-05 16:23:49 +0530

    -

---
+++ nearest2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int n,d=2,s[100],m[100],i;
+	printf("Enter a number");
+	scanf("%d",&n);
+            for(i=0;i<n;i++)
+            {
+            	m=m*2;
+            	printf("\n %d",m);
+            }
+            }
+            	
+ 
